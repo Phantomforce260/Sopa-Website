6c901a163e348e0432f970517ff178c210e31f1e
Adri <aakeem0920@gmail.com>
2025-01-04 12:43:36 -0500

    Initial Commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php require('partials/header.php') ?>
+        <title>Solo Sopas</title>
+    </head>
+
+    <body>
+        <section class="home-header">
+            <nav id="home-header-nav">
+                <!-- <a href="/"><img src=""></a> -->
+                <div id="red-container">
+                    <a href="#">LOGO</a>
+                </div>
+
+                <div id="blue-container">
+                    <div class="nav-links header" id="navLinks">
+                        <!-- <i class="bx bx-x" onclick="hideMenu()"></i> -->
+                        <ul>
+                            <li><a href="#">About Us</a></li>
+                            <li><a href="#">Menu</a></li>
+                            <li><a href="#">Opinions</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+
+            <div id="main-content" class="text-box">
+                <h1>Bienvenidos!</h1>
+                <a href="#">Check Out Our Menu!</a>
+            </div>
+
+        </section>
+    </body>
+</html>
