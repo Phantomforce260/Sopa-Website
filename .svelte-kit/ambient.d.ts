7209a730f567f02e1ca6586ba5b20b07c102bd72
Adri <aakeem0920@gmail.com>
2025-02-08 22:10:15 -0500

    Adding new images

---
+++ .svelte-kit/ambient.d.ts
@@ -0,0 +1,217 @@
+
+// this file is generated — do not edit it
+
+
+/// <reference types="@sveltejs/kit" />
+
+/**
+ * Environment variables [loaded by Vite](https://vitejs.dev/guide/env-and-mode.html#env-files) from `.env` files and `process.env`. Like [`$env/dynamic/private`](https://svelte.dev/docs/kit/$env-dynamic-private), this module cannot be imported into client-side code. This module only includes variables that _do not_ begin with [`config.kit.env.publicPrefix`](https://svelte.dev/docs/kit/configuration#env) _and do_ start with [`config.kit.env.privatePrefix`](https://svelte.dev/docs/kit/configuration#env) (if configured).
+ * 
+ * _Unlike_ [`$env/dynamic/private`](https://svelte.dev/docs/kit/$env-dynamic-private), the values exported from this module are statically injected into your bundle at build time, enabling optimisations like dead code elimination.
+ * 
+ * ```ts
+ * import { API_KEY } from '$env/static/private';
+ * ```
+ * 
+ * Note that all environment variables referenced in your code should be declared (for example in an `.env` file), even if they don't have a value until the app is deployed:
+ * 
+ * ```
+ * MY_FEATURE_FLAG=""
+ * ```
+ * 
+ * You can override `.env` values from the command line like so:
+ * 
+ * ```bash
+ * MY_FEATURE_FLAG="enabled" npm run dev
+ * ```
+ */
+declare module '$env/static/private' {
+	export const SHELL: string;
+	export const npm_command: string;
+	export const LSCOLORS: string;
+	export const QT_ACCESSIBILITY: string;
+	export const COLORTERM: string;
+	export const HYPRLAND_CMD: string;
+	export const LESS: string;
+	export const XDG_BACKEND: string;
+	export const CLUTTER_BACKEND: string;
+	export const QT_WAYLAND_DISABLE_WINDOWDECORATION: string;
+	export const NODE: string;
+	export const SSH_AUTH_SOCK: string;
+	export const npm_config_local_prefix: string;
+	export const DESKTOP_SESSION: string;
+	export const ELECTRON_OZONE_PLATFORM_HINT: string;
+	export const KITTY_PID: string;
+	export const GTK_MODULES: string;
+	export const XDG_SEAT: string;
+	export const PWD: string;
+	export const XDG_SESSION_DESKTOP: string;
+	export const LOGNAME: string;
+	export const QT_QPA_PLATFORMTHEME: string;
+	export const XDG_SESSION_TYPE: string;
+	export const _: string;
+	export const KITTY_PUBLIC_KEY: string;
+	export const HOME: string;
+	export const USERNAME: string;
+	export const LANG: string;
+	export const LS_COLORS: string;
+	export const _JAVA_AWT_WM_NONREPARENTING: string;
+	export const XDG_CURRENT_DESKTOP: string;
+	export const npm_package_version: string;
+	export const WAYLAND_DISPLAY: string;
+	export const KITTY_WINDOW_ID: string;
+	export const QT_QPA_PLATFORM: string;
+	export const npm_lifecycle_script: string;
+	export const XDG_SESSION_CLASS: string;
+	export const TERMINFO: string;
+	export const TERM: string;
+	export const npm_package_name: string;
+	export const ZSH: string;
+	export const USER: string;
+	export const HYPRLAND_INSTANCE_SIGNATURE: string;
+	export const DISPLAY: string;
+	export const npm_lifecycle_event: string;
+	export const SHLVL: string;
+	export const MOZ_ENABLE_WAYLAND: string;
+	export const GSM_SKIP_SSH_AGENT_WORKAROUND: string;
+	export const PAGER: string;
+	export const XDG_VTNR: string;
+	export const XDG_SESSION_ID: string;
+	export const npm_config_user_agent: string;
+	export const npm_execpath: string;
+	export const XDG_RUNTIME_DIR: string;
+	export const npm_package_json: string;
+	export const BUN_INSTALL: string;
+	export const QT_AUTO_SCREEN_SCALE_FACTOR: string;
+	export const XDG_DATA_DIRS: string;
+	export const GDK_BACKEND: string;
+	export const PATH: string;
+	export const GDMSESSION: string;
+	export const DBUS_SESSION_BUS_ADDRESS: string;
+	export const ENVMAN_LOAD: string;
+	export const QT_SCALE_FACTOR: string;
+	export const KITTY_INSTALLATION_DIR: string;
+	export const npm_node_execpath: string;
+	export const OLDPWD: string;
+	export const NODE_ENV: string;
+}
+
+/**
+ * Similar to [`$env/static/private`](https://svelte.dev/docs/kit/$env-static-private), except that it only includes environment variables that begin with [`config.kit.env.publicPrefix`](https://svelte.dev/docs/kit/configuration#env) (which defaults to `PUBLIC_`), and can therefore safely be exposed to client-side code.
+ * 
+ * Values are replaced statically at build time.
+ * 
+ * ```ts
+ * import { PUBLIC_BASE_URL } from '$env/static/public';
+ * ```
+ */
+declare module '$env/static/public' {
+	
+}
+
+/**
+ * This module provides access to runtime environment variables, as defined by the platform you're running on. For example if you're using [`adapter-node`](https://github.com/sveltejs/kit/tree/main/packages/adapter-node) (or running [`vite preview`](https://svelte.dev/docs/kit/cli)), this is equivalent to `process.env`. This module only includes variables that _do not_ begin with [`config.kit.env.publicPrefix`](https://svelte.dev/docs/kit/configuration#env) _and do_ start with [`config.kit.env.privatePrefix`](https://svelte.dev/docs/kit/configuration#env) (if configured).
+ * 
+ * This module cannot be imported into client-side code.
+ * 
+ * Dynamic environment variables cannot be used during prerendering.
+ * 
+ * ```ts
+ * import { env } from '$env/dynamic/private';
+ * console.log(env.DEPLOYMENT_SPECIFIC_VARIABLE);
+ * ```
+ * 
+ * > In `dev`, `$env/dynamic` always includes environment variables from `.env`. In `prod`, this behavior will depend on your adapter.
+ */
+declare module '$env/dynamic/private' {
+	export const env: {
+		SHELL: string;
+		npm_command: string;
+		LSCOLORS: string;
+		QT_ACCESSIBILITY: string;
+		COLORTERM: string;
+		HYPRLAND_CMD: string;
+		LESS: string;
+		XDG_BACKEND: string;
+		CLUTTER_BACKEND: string;
+		QT_WAYLAND_DISABLE_WINDOWDECORATION: string;
+		NODE: string;
+		SSH_AUTH_SOCK: string;
+		npm_config_local_prefix: string;
+		DESKTOP_SESSION: string;
+		ELECTRON_OZONE_PLATFORM_HINT: string;
+		KITTY_PID: string;
+		GTK_MODULES: string;
+		XDG_SEAT: string;
+		PWD: string;
+		XDG_SESSION_DESKTOP: string;
+		LOGNAME: string;
+		QT_QPA_PLATFORMTHEME: string;
+		XDG_SESSION_TYPE: string;
+		_: string;
+		KITTY_PUBLIC_KEY: string;
+		HOME: string;
+		USERNAME: string;
+		LANG: string;
+		LS_COLORS: string;
+		_JAVA_AWT_WM_NONREPARENTING: string;
+		XDG_CURRENT_DESKTOP: string;
+		npm_package_version: string;
+		WAYLAND_DISPLAY: string;
+		KITTY_WINDOW_ID: string;
+		QT_QPA_PLATFORM: string;
+		npm_lifecycle_script: string;
+		XDG_SESSION_CLASS: string;
+		TERMINFO: string;
+		TERM: string;
+		npm_package_name: string;
+		ZSH: string;
+		USER: string;
+		HYPRLAND_INSTANCE_SIGNATURE: string;
+		DISPLAY: string;
+		npm_lifecycle_event: string;
+		SHLVL: string;
+		MOZ_ENABLE_WAYLAND: string;
+		GSM_SKIP_SSH_AGENT_WORKAROUND: string;
+		PAGER: string;
+		XDG_VTNR: string;
+		XDG_SESSION_ID: string;
+		npm_config_user_agent: string;
+		npm_execpath: string;
+		XDG_RUNTIME_DIR: string;
+		npm_package_json: string;
+		BUN_INSTALL: string;
+		QT_AUTO_SCREEN_SCALE_FACTOR: string;
+		XDG_DATA_DIRS: string;
+		GDK_BACKEND: string;
+		PATH: string;
+		GDMSESSION: string;
+		DBUS_SESSION_BUS_ADDRESS: string;
+		ENVMAN_LOAD: string;
+		QT_SCALE_FACTOR: string;
+		KITTY_INSTALLATION_DIR: string;
+		npm_node_execpath: string;
+		OLDPWD: string;
+		NODE_ENV: string;
+		[key: `PUBLIC_${string}`]: undefined;
+		[key: `${string}`]: string | undefined;
+	}
+}
+
+/**
+ * Similar to [`$env/dynamic/private`](https://svelte.dev/docs/kit/$env-dynamic-private), but only includes variables that begin with [`config.kit.env.publicPrefix`](https://svelte.dev/docs/kit/configuration#env) (which defaults to `PUBLIC_`), and can therefore safely be exposed to client-side code.
+ * 
+ * Note that public dynamic environment variables must all be sent from the server to the client, causing larger network requests — when possible, use `$env/static/public` instead.
+ * 
+ * Dynamic environment variables cannot be used during prerendering.
+ * 
+ * ```ts
+ * import { env } from '$env/dynamic/public';
+ * console.log(env.PUBLIC_DEPLOYMENT_SPECIFIC_VARIABLE);
+ * ```
+ */
+declare module '$env/dynamic/public' {
+	export const env: {
+		[key: `PUBLIC_${string}`]: string | undefined;
+	}
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,37 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<style>
+    .background {
+        background-image: url(./images/background_home.png);
+    }
+
+    .header-nav {
+        background: rgba(0, 0, 0, 0.4);
+    }
+
+    h1 {
+        font-family: segoescript;
+        color: white;
+    }
+
+    a, p {
+        font-family: playwrite-moderne;
+        color: white;
+    }
+</style>
+
+<section class="background">
+    <div class="header-nav">
+        <a href="./"><img src="./images/Logo.png" alt=""></a>
+        <div class="nav-links">
+            <ul>
+                <li><a href="./">About Us</a></li>
+                <li><a href="./">Menu</a></li>
+                <li><a href="./">Opinions</a></li>
+            </ul>
+        </div>
+    </div>
+
+    <div id="main-content">
+        <h1>Bienvenidos!</h1>
+        <p>Check out our Menu.</p>
+    </div>
+</section>
